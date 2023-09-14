2aa0a488d22c788c1e08b0a1a34b7fd22d8ec00f
Paula Camargo <paulitacv25@gmail.com>
2023-09-14 16:39:25 -0700

    fix pr comments

--- src/extension/debugger/debugPort/portAttributesProvider.ts
+++ src/extension/debugger/debugPort/portAttributesProvider.ts
@@ -2,19 +2,13 @@
  * Copyright (C) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------*/
 
-import {
-    CancellationToken,
-    PortAttributes,
-    PortAttributesProvider,
-    PortAutoForwardAction,
-    ProviderResult,
-} from 'vscode';
+import { CancellationToken, PortAttributes, PortAttributesProvider, ProviderResult } from 'vscode';
 
 export class DebugPortAttributesProvider implements PortAttributesProvider {
     public providePortAttributes(
         _attributes: { port: number; pid?: number; commandLine?: string },
         _token: CancellationToken,
     ): ProviderResult<PortAttributes> {
-        return new PortAttributes(PortAutoForwardAction.Ignore);
+        return undefined;
     }
 }
