a8b2b83c777df45e5b4237d8b2b63420eb22c358
paulacamargo25 <paulitacv25@gmail.com>
2024-02-22 17:04:48 -0800

    Add auto start browser as default value (#229)
    * Add autoStartBrowser
    
    * fix tests
    
    * fix lint
    
    * fix tests

--- src/test/unittest/configuration/providers/djangoLaunch.unit.test.ts
+++ src/test/unittest/configuration/providers/djangoLaunch.unit.test.ts
@@ -109,6 +109,7 @@ suite('Debugging - Configuration Provider Django', () => {
             program: 'hello',
             args: ['runserver'],
             django: true,
+            autoStartBrowser: false,
         };
 
         expect(state.config).to.be.deep.equal(config);
@@ -129,6 +130,7 @@ suite('Debugging - Configuration Provider Django', () => {
             program: defaultProgram,
             args: ['runserver'],
             django: true,
+            autoStartBrowser: false,
         };
 
         expect(state.config).to.be.deep.equal(config);
