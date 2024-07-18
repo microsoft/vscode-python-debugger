481ccdb42cef3ef7205c42283171677e68819032
Paula <paulitacv25@gmail.com>
2024-07-18 16:51:21 -0700

    Read launch config from devcontainers (#388)
    * Use workspace when getting the launch.json
    
    * fix local vsix creation

--- src/extension/debugger/configuration/launch.json/launchJsonReader.ts
+++ src/extension/debugger/configuration/launch.json/launchJsonReader.ts
@@ -11,7 +11,7 @@ export async function getConfigurationsForWorkspace(workspace: WorkspaceFolder):
     const filename = path.join(workspace.uri.fsPath, '.vscode', 'launch.json');
     if (!(await fs.pathExists(filename))) {
         // Check launch config in the workspace file
-        const codeWorkspaceConfig = getConfiguration('launch');
+        const codeWorkspaceConfig = getConfiguration('launch', workspace);
         if (!codeWorkspaceConfig.configurations || !Array.isArray(codeWorkspaceConfig.configurations)) {
             return [];
         }
