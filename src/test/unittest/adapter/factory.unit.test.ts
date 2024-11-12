d3f94b4639634aed0750bd6411a33e77dbe65214
eleanorjboyd <eleanorboyd@microsoft.com>
2024-11-12 09:12:03 -0800

    error fix

--- src/test/unittest/adapter/factory.unit.test.ts
+++ src/test/unittest/adapter/factory.unit.test.ts
@@ -294,7 +294,7 @@ suite('Debugging - Adapter Factory', () => {
     test('Add quotes to interpreter path with spaces', async () => {
         const customAdapterPath = path.join('custom', 'debug', 'adapter', 'customAdapterPath');
         const session = createSession({ debugAdapterPath: customAdapterPath });
-        const interpreterPathSpaces = path.join('/path', 'to', 'python interpreter with spaces');
+        const interpreterPathSpaces = path.join('path', 'to', 'python interpreter with spaces');
         const interpreterPathSpacesQuoted = `"${interpreterPathSpaces}"`;
         const debugExecutable = new DebugAdapterExecutable(interpreterPathSpacesQuoted, [customAdapterPath]);
 
