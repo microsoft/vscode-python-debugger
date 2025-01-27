2323242ffedcff5fa068cba532e304e4855191f0
eleanorjboyd <eleanorboyd@microsoft.com>
2025-01-27 10:53:19 -0800

    switch to using python3 in the script for no-config debugging

--- bundled/scripts/noConfigScripts/debugpy.bat
+++ bundled/scripts/noConfigScripts/debugpy.bat
@@ -1,3 +1,3 @@
 @echo off
 :: Bat script
-python %BUNDLED_DEBUGPY_PATH% --listen 0 --wait-for-client %*
+python3 %BUNDLED_DEBUGPY_PATH% --listen 0 --wait-for-client %*
--- bundled/scripts/noConfigScripts/debugpy.ps1
+++ bundled/scripts/noConfigScripts/debugpy.ps1
@@ -1,2 +1,2 @@
 # PowerShell script
-python $env:BUNDLED_DEBUGPY_PATH --listen 0 --wait-for-client $args
+python3 $env:BUNDLED_DEBUGPY_PATH --listen 0 --wait-for-client $args
