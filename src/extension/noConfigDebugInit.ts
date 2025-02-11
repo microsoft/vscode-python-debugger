0e01d47e64a79ebdc47e1098d393258cb7f5ff14
eleanorjboyd <eleanorboyd@microsoft.com>
2025-02-11 10:07:46 -0800

    rename

--- src/extension/noConfigDebugInit.ts
+++ src/extension/noConfigDebugInit.ts
@@ -37,18 +37,18 @@ export async function registerNoConfigDebug(
 
     // create a temp directory for the noConfigDebugAdapterEndpoints
     // file path format: extPath/.noConfigDebugAdapterEndpoints/endpoint-stableWorkspaceHash.txt
-    let workspaceUri = workspace.workspaceFile?.fsPath;
-    if (!workspaceUri) {
-        workspaceUri = workspace.workspaceFolders?.map((e) => e.uri.fsPath).join(';');
+    let workspaceString = workspace.workspaceFile?.fsPath;
+    if (!workspaceString) {
+        workspaceString = workspace.workspaceFolders?.map((e) => e.uri.fsPath).join(';');
     }
-    if (!workspaceUri) {
+    if (!workspaceString) {
         traceError('No workspace folder found');
         return Promise.resolve(new Disposable(() => {}));
     }
 
     // create a stable hash for the workspace folder, reduce terminal variable churn
     const hash = crypto.createHash('sha256');
-    hash.update(workspaceUri.toString());
+    hash.update(workspaceString.toString());
     const stableWorkspaceHash = hash.digest('hex').slice(0, 16);
 
     const tempDirPath = path.join(extPath, '.noConfigDebugAdapterEndpoints');
