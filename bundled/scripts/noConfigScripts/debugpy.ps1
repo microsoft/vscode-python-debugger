0c25520e39b2a6e9a1d0b368bb712f2462b2f0d6
eleanorjboyd <26030610+eleanorjboyd@users.noreply.github.com>
2025-02-12 09:46:39 -0800

    fix powershell script for powershell no-config debug

--- bundled/scripts/noConfigScripts/debugpy.ps1
+++ bundled/scripts/noConfigScripts/debugpy.ps1
@@ -1,5 +1,6 @@
 # PowerShell script
-if ($PSVersionTable.OS -match "Windows") {
+$os = [System.Environment]::OSVersion.Platform
+if ($os -eq [System.PlatformID]::Win32NT) {
     python $env:BUNDLED_DEBUGPY_PATH --listen 0 --wait-for-client $args
 } else {
     python3 $env:BUNDLED_DEBUGPY_PATH --listen 0 --wait-for-client $args
