b6487bd8d2e9f02ddfb180735b6a5fda9e71f611
eleanorjboyd <26030610+eleanorjboyd@users.noreply.github.com>
2025-02-20 11:41:44 -0800

    update for all shell types

--- bundled/scripts/noConfigScripts/debugpy.bat
+++ bundled/scripts/noConfigScripts/debugpy.bat
@@ -1,3 +1,4 @@
 @echo off
 :: Bat script
+set DEBUGPY_ADAPTER_ENDPOINTS=%VSCODE_DEBUGPY_ADAPTER_ENDPOINTS%
 python %BUNDLED_DEBUGPY_PATH% --listen 0 --wait-for-client %*
--- bundled/scripts/noConfigScripts/debugpy.ps1
+++ bundled/scripts/noConfigScripts/debugpy.ps1
@@ -1,4 +1,5 @@
 # PowerShell script
+$env:DEBUGPY_ADAPTER_ENDPOINTS = $env:VSCODE_DEBUGPY_ADAPTER_ENDPOINTS
 $os = [System.Environment]::OSVersion.Platform
 if ($os -eq [System.PlatformID]::Win32NT) {
     python $env:BUNDLED_DEBUGPY_PATH --listen 0 --wait-for-client $args
