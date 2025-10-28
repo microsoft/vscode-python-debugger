febb9d3f0764bc2c185d758c8bd093ce68ec2cb7
ZA139 <Lwq_Charles_Liu@outlook.com>
2025-10-28 16:36:24 +0800

    Delete processSelect.ps1

--- bundled/scripts/noConfigScripts/processSelect.ps1
+++
@@ -1,4 +0,0 @@
-# PowerShell script
-$env:DEBUGPY_ADAPTER_ENDPOINTS = $env:VSCODE_DEBUGPY_ADAPTER_ENDPOINTS
-$os = [System.Environment]::OSVersion.Platform
-Get-WmiObject Win32_Process | Select-Object Name,CommandLine,ProcessId | Format-List
