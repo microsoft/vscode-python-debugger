1da32c996fbee3f831b9c9abb2bf2f040d4a65c6
Zentrik <llvm.zentrik@gmail.com>
2026-01-07 06:40:45 +0000

    Update comment in debugpy.sh for clarity
    Clarified the purpose of the debugpy script in the comment.

--- bundled/scripts/noConfigScripts/debugpy.sh
+++ bundled/scripts/noConfigScripts/debugpy.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-# Bash script
+# Bash script - a copy of `debugpy` for cases where the user has a conflicting `debugpy` in their `PATH`.
 export DEBUGPY_ADAPTER_ENDPOINTS=$VSCODE_DEBUGPY_ADAPTER_ENDPOINTS
 python3 $BUNDLED_DEBUGPY_PATH --listen 0 --wait-for-client $@
